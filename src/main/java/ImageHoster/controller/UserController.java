13213a130f6a3fc458dc034b3dbfd8acf0eb346c
UGjaguar <rakesh.pujari1109@gmail.com>
2020-12-22 03:10:09 +0800

    error fixes

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -40,11 +40,12 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
+    public String registerUser(User user, Model model) {
         //userService.registerUser(user);
         //return "redirect:/users/login";
         String password = user.getPassword();
         String passwordValidationError = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+
         Boolean passwordValidation = passwordValidation(password);
         String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
         if (passwordValidation) {
